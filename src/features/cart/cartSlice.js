d8b32f3e5dd3d2d8af6bc89a4f4f0bbbcd941958
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-04-06 18:46:40 +0300

    Add removeItem, increase,decrease,calculat reducer

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -3,7 +3,7 @@ import cartItems from "../../cartItems";
 
 const initialState = {
     cartItem : cartItems,
-    amount:4,
+    amount:0,
     total:0,
     isLoading:true
 }
@@ -16,10 +16,32 @@ const cartSlice = createSlice({
     reducers:{
         clearCart: (state)=>{
             state.cartItem = []; 
+        },
+        removeItem: (state, action) =>{
+            const itemId = action.payload
+            state.cartItem = state.cartItem.filter((item)=> item.id !== itemId)
+        },
+        increase: (state, { payload }) => {
+            const increment = state.cartItem.find((item)=>item.id === payload)
+            increment.amount = increment.amount + 1;
+        },
+        decrease: (state, { payload }) => {
+            const decrement =state.cartItem.find((item)=>item.id === payload)
+            decrement.amount = decrement.amount - 1;
+        },
+        calculateTotals: (state) => {
+            let amount=0 ;
+            let total =0;
+            state.cartItem.forEach((item) => {
+                amount += item.amount;
+                total += item.amount * item.price;
+            })
+            state.amount = amount;
+            state.total = total;
         }
     }
 })
 
 // console.log(cartSlice)
-export const {clearCart} = cartSlice.actions
+export const {clearCart, removeItem, increase, decrease, calculateTotals} = cartSlice.actions
 export default cartSlice.reducer
