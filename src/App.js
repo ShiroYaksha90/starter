b33334e6bd5f474e4cfce665b107947af7505957
Basim Aljazaeri <30289772+ShiroYaksha90@users.noreply.github.com>
2023-04-06 18:48:56 +0300

    Add redux functionality to App.js file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { useEffect } from "react";
 import Navbar from "./components/Navbar"
 import CartContainer from './components/cartContainer';
-
+import { useSelector, useDispatch } from "react-redux";
+import { calculateTotals } from "./features/cart/cartSlice";
 function App() {
+  const { cartItem } = useSelector((state) => state.cart);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItem]);
   return <main>
     <Navbar/>
     <CartContainer/>
